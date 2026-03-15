59f7570400ba0f4bb6ff617cc1964807f6bd0b89
Try Ajitiono <ballinst@gmail.com>
2026-03-15 05:36:38 +0000

    not sure

--- app/root.tsx
+++ app/root.tsx
@@ -47,7 +47,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           dangerouslySetInnerHTML={{
             __html: `
 <iframe
-  src="https://www.googletagmanager.com/ns.html?id= G-NVZE75RYKR"
+  src="https://www.googletagmanager.com/ns.html?id=G-NVZE75RYKR"
   height="0"
   width="0"
   style="display: none; visibility: hidden"
